3651957d362a8eae1ac30a35e2fcb783e15ab9bf
JohannesMRS <mariojr8304@gmail.com>
2026-07-10 22:36:31 +0700

    Feat: Callback Practice Exercises

--- 03-asynchronous-api/01-callback/script.js
+++ 03-asynchronous-api/01-callback/script.js
@@ -35,57 +35,130 @@
 
 
 
-function prosesNama(nama, callback){
-    const hasil = nama.toUpperCase();
-    callback(hasil);
+// function prosesNama(nama, callback){
+//     const hasil = nama.toUpperCase();
+//     callback(hasil);
+// }
+
+// prosesNama("Johannes", (hasil)=>{
+//     console.log("Nama: ", hasil);
+// })
+
+
+// function hitungTotalBelanja(keranjang, callback){
+//     let total = 0;
+
+//     for(let harga of keranjang){
+//         total += harga;
+//     }
+
+//     callback(total);
+// }
+
+// const keranjang = [15000, 20000, 12000];
+// hitungTotalBelanja(keranjang, (total)=>{
+//     console.log(`Hasil: ${total}`);
+// })
+
+
+// function dataUser(userId, callback){
+//     if(userId){
+//         setTimeout(function(){
+//             callback({id: userId, username: "dev-medan"});
+//         }, 2000)
+//     }
+// }
+
+// console.log("Memulai pencarian user...")
+// dataUser(101, (hasil)=>{
+//     console.log("Data ditemukan: ", hasil);
+// })
+
+// function cekStokBarang(jumlahBeli, callback){
+//     if(jumlahBeli > 10){
+//         callback("Stok tidak mencukupi", null);
+//     }else{
+//         callback(null, "Pesanan di proses");
+//     }
+// }
+
+// cekStokBarang(9, (err, data)=>{
+//     if(err){
+//         console.log("Error: ", err);
+//     }else{
+//         console.log("Sukses: ", data);
+//     }
+// })
+
+
+// function cekUser(username, callback){
+//     if(username === "Johannes"){
+//         callback(101)
+//     }
+// }
+
+// function ambilKeranjang(userId, callback){
+//     let hargas = [20000, 30000, 50000];
+//     let totalAwal = 0;
+//     for(let harga of hargas){
+//         totalAwal += harga;
+//     }
+//     callback(totalAwal);
+// }
+
+// function hitungDiskon(totalAwal, callback){
+//     let totalAkhir = 0;
+//     if(totalAwal >= 100000){
+//         totalAkhir = totalAwal - (totalAwal * 10/100);
+//         callback(totalAkhir);
+//     }
+// }
+
+// cekUser("Johannes", function(responseUser){
+//     ambilKeranjang(responseUser, function(responseKeranjang){
+//         hitungDiskon(responseKeranjang, function(responseDiskon){
+//             console.log(`User ${responseUser} harus membayar Rp${responseDiskon} setelah diskon`)
+//         })
+//     })
+// })
+
+// function filterFormatProduk(daftarProduk, callback){
+//     const produkFiltered = daftarProduk.filter((item)=>item.stok > 0);
+//     const mapped = produkFiltered.map((item)=>{
+//         return{
+//             nama: item.nama.toUpperCase(),
+//             harga: item.harga
+//         }
+//     });
+//     callback(mapped);
+// }
+
+// const daftarProduk = [
+//     { id: 1, nama: "kopi hitam", harga: 12000, stok: 5 },
+//     { id: 2, nama: "roti bakar", harga: 15000, stok: 0 },
+//     { id: 3, nama: "susu segar", harga: 10000, stok: 3 }
+// ];
+
+
+// filterFormatProduk(daftarProduk, (hasil)=>{
+//     console.log(hasil);
+// })
+
+function prosesData(callback) {
+    console.log("1");
+    
+    setTimeout(() => {
+        console.log("2");
+        callback();
+    }, 0);
+    
+    console.log("3");
 }
 
-prosesNama("Johannes", (hasil)=>{
-    console.log("Nama: ", hasil);
-})
+console.log("4");
 
+prosesData(() => {
+    console.log("5");
+});
 
-function hitungTotalBelanja(keranjang, callback){
-    let total = 0;
-
-    for(let harga of keranjang){
-        total += harga;
-    }
-
-    callback(total);
-}
-
-const keranjang = [15000, 20000, 12000];
-hitungTotalBelanja(keranjang, (total)=>{
-    console.log(`Hasil: ${total}`);
-})
-
-
-function dataUser(userId, callback){
-    if(userId){
-        setTimeout(function(){
-            callback({id: userId, username: "dev-medan"});
-        }, 2000)
-    }
-}
-
-console.log("Memulai pencarian user...")
-dataUser(101, (hasil)=>{
-    console.log("Data ditemukan: ", hasil);
-})
-
-function cekStokBarang(jumlahBeli, callback){
-    if(jumlahBeli > 10){
-        callback("Stok tidak mencukupi", null);
-    }else{
-        callback(null, "Pesanan di proses");
-    }
-}
-
-cekStokBarang(9, (err, data)=>{
-    if(err){
-        console.log("Error: ", err);
-    }else{
-        console.log("Sukses: ", data);
-    }
-})
+console.log("6");
