26d9a19540bb5a49a63040821c286bee7b69c7c7
JohannesMRS <mariojr8304@gmail.com>
2026-07-12 22:11:15 +0700

    Feat: Solving Promise Problem

--- 03-asynchronous-api/03-promise/soal.js
+++ 03-asynchronous-api/03-promise/soal.js
@@ -1,35 +1,90 @@
-function cekUmur(umur){
+// function cekUmur(umur){
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             if(umur < 17){
+//                 reject("Maaf, Kamu Belum Cukup Umur!")
+//             }else{
+//                 resolve("Selamat, Kamu Sudah Cukup Umur");                
+//             }
+//         }, 1000)
+//     })
+// }
+
+// function pesanTambahan(umur){
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             if(!umur){
+//                 reject("Pesan Tidak Dikirimkan")
+//             }else{
+//                 resolve("Silahkan Lanjut Ke Tahap Berikutnya...")
+//             }
+//         }, 1000)
+//     })
+// }
+
+
+// async function cekUmurInfo(umurUser){
+//     try{
+//         const umur = await cekUmur(umurUser);
+//         const status = await pesanTambahan(umur);
+//         console.log(`${umur} \n${status}`);
+//     }catch(err){console.log(err)}
+// }
+
+// cekUmurInfo(20);
+// cekUmurInfo(10);
+
+
+
+function validasiApiKey(key){
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
-            if(umur < 17){
-                reject("Maaf, Kamu Belum Cukup Umur!")
+            if(key === "RAHASIA123"){
+                resolve("Akses Diterima");
             }else{
-                resolve("Selamat, Kamu Sudah Cukup Umur");                
+                reject("API Key Tidak Valid");
+            }
+        }, 1000);
+    })
+}
+
+
+validasiApiKey("RAHASIA123").then((result)=>{
+    console.log(`Status: ${result}`);
+}).catch((err)=>{
+    console.log(`Status: ${err}`);
+})
+
+function cekStok(barang){
+    return new Promise((resolve, reject)=>{
+        console.log(`Cek Stok ${barang}`);
+        setTimeout(()=>{
+            if(barang === "Kopi"){
+                resolve(true);
+            }else{
+                reject("Barang Habis!");
             }
         }, 1000)
     })
 }
 
-function pesanTambahan(umur){
+function buatTagihan(statusStok){
     return new Promise((resolve, reject)=>{
+        console.log("Memproses Permintaan....")
         setTimeout(()=>{
-            if(!umur){
-                reject("Pesan Tidak Dikirimkan")
+            if(statusStok===true){
+                resolve(15000);
             }else{
-                resolve("Silahkan Lanjut Ke Tahap Berikutnya...")
+                reject("Stok Habis!")
             }
-        }, 1000)
+        }, 1000);
     })
 }
 
-
-async function cekUmurInfo(umurUser){
-    try{
-        const umur = await cekUmur(umurUser);
-        const status = await pesanTambahan(umur);
-        console.log(`${umur} \n${status}`);
-    }catch(err){console.log(err)}
-}
-
-cekUmurInfo(20);
-cekUmurInfo(10);
+cekStok("Kopi").then((statusBarang)=>{
+    return buatTagihan(statusBarang);
+}).then((hargaTotal)=>{
+    console.log(`Harga: ${hargaTotal}`);
+}).catch((err)=>{
+    console.log(err);
+})
