84d326440269e2d370d17ff45e50f5412ccbf3cf
JohannesMRS <mariojr8304@gmail.com>
2026-07-14 09:06:47 +0700

    Feat: Fetch User Followers and Repositories Using GitHub API

--- 03-asynchronous-api/06-async-await/implement.js
+++ 03-asynchronous-api/06-async-await/implement.js
@@ -15,16 +15,51 @@
 // getUser();
 
 
-async function getFollowers(username){
+// async function getFollowers(username){
+//     try{
+//         const response = await fetch(`https://api.github.com/users/${username}/repos`);
+//         const followers = await response.json();
+//         followers.forEach((follower)=>{
+//             console.log(follower.name);
+//         })
+//     }catch(error){
+//         console.log(error);
+//     }
+// }
+
+// getFollowers('JohannesMRS');
+
+const followers = document.getElementById('follower');
+const repository = document.getElementById('repo');
+
+async function getFollowers(){
     try{
-        const response = await fetch(`https://api.github.com/users/${username}/repos`);
-        const followers = await response.json();
-        followers.forEach((follower)=>{
-            console.log(follower.name);
+        const response = await fetch('https://api.github.com/users/JohannesMRS/followers');
+        const repo = await fetch('https://api.github.com/users/JohannesMRS/repos');
+
+        if(!response.ok || !repo.ok){
+            throw new Error("Error Status: ", response.status);
+        }
+
+        const dataFollowers = await response.json();
+        const dataRepo = await repo.json();
+
+        let htmlString = '';
+        let reposString = ''
+
+        dataFollowers.forEach((item)=>{
+            htmlString += `<li>${item.login}</li>`
         })
+
+        dataRepo.forEach((item)=>{
+            reposString += `<li>${item.name}</li>`;
+        })
+
+        followers.innerHTML = htmlString;
+        repository.innerHTML = reposString;
     }catch(error){
-        console.log(error);
+        alert("Error", error.message);
     }
 }
 
-getFollowers('JohannesMRS');
+getFollowers();
