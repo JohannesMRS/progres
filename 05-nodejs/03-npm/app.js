a3b2ff9303a7d578a76968675207816ef0df0356
JohannesMRS <mariojr8304@gmail.com>
2026-07-16 11:36:32 +0700

    Feat: Using Some Package From NPM

--- 05-nodejs/03-npm/app.js
+++ 05-nodejs/03-npm/app.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+import chalk from 'chalk';
+import validator from 'validator';
+
+console.log(validator.isEmail('j@gmail.com'));
+console.log(validator.isMobilePhone('081266254708', 'id-ID'));
+
+console.log(chalk.red('Hellow'))
+
+
